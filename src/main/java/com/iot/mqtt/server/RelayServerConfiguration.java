6ccc2f36ebd492fca9bb65ff452b5fe700f0a216
liangjiajun <Xlink789456>
2022-07-27 15:24:25 +0800

    #feat：先屏蔽转发操作

--- src/main/java/com/iot/mqtt/server/RelayServerConfiguration.java
+++ src/main/java/com/iot/mqtt/server/RelayServerConfiguration.java
@@ -1,8 +1,10 @@
+/*
 package com.iot.mqtt.server;
 
 import com.iot.mqtt.config.MqttConfig;
 import com.iot.mqtt.message.handler.AutoFlushHandler;
 import com.iot.mqtt.message.handler.MqttMessageHandler;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.epoll.EpollEventLoopGroup;
@@ -30,75 +32,52 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.util.concurrent.TimeUnit;
 
+*/
 /**
  * @author liangjiajun
- */
+ *//*
+
 @Slf4j
 @Configuration
 public class RelayServerConfiguration {
 
     @Autowired
     private MqttConfig mqttConfig;
+
     @Autowired
     private MqttMessageHandler mqttMessageHandler;
-    /**
+    */
+/**
      * ssl
-     */
+     *//*
+
     private SslContext sslContext;
-    /**
+    */
+/**
      * boss 线程池
-     */
+     *//*
+
     private EventLoopGroup bossGroup;
-    /**
+    */
+/**
      * work 线程池
-     */
+     *//*
+
     private EventLoopGroup workerGroup;
 
     @Bean
     public void start() throws Exception {
-        log.info("relay server is listening on port {} ", mqttConfig.getPort());
         relayServer();
+        log.info("relay server is listening on port {} ", mqttConfig.getPort());
+        relayClient();
+        log.info("relay client is listening on port {} ", mqttConfig.getPort());
     }
 
-    /**
-     * 转发客户端
-     */
-    private void relayClient() {
-        ServerBootstrap bootstrap = new ServerBootstrap().group(bossGroup, workerGroup).channel(mqttConfig.getUseEpoll() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
-                .handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel channel) {
-                        ChannelPipeline channelPipeline = channel.pipeline();
-                        // Netty心跳机制
-                        channelPipeline.addLast("idle", new IdleStateHandler(0, 0, mqttConfig.getKeepAlive()));
-                        // Netty提供的SSL处理
-                        if (mqttConfig.getSsl()) {
-                            SSLEngine sslEngine = sslContext.newEngine(channel.alloc());
-                            sslEngine.setUseClientMode(false);        // 服务端模式
-                            sslEngine.setNeedClientAuth(false);        // 不需要验证客户端
-                            channelPipeline.addLast("ssl", new SslHandler(sslEngine));
-                        }
-                        channelPipeline.addLast("autoflush", new AutoFlushHandler(1, TimeUnit.SECONDS));
-                        channelPipeline.addLast("decoder", new MqttDecoder());
-                        channelPipeline.addLast("encoder", MqttEncoder.INSTANCE);
-                        channelPipeline.addLast("broker", mqttMessageHandler);
-                    }
-                })
-                .option(ChannelOption.SO_BACKLOG, mqttConfig.getSoBacklog())
-                .option(ChannelOption.SO_REUSEADDR, true)
-                .childOption(ChannelOption.TCP_NODELAY, true)
-                .childOption(ChannelOption.SO_KEEPALIVE, mqttConfig.getSoKeepAlive())
-                .childOption(ChannelOption.WRITE_BUFFER_WATER_MARK, WriteBufferWaterMark.DEFAULT);
-        if (Strings.isNotBlank(mqttConfig.getHost())) {
-            bootstrap.bind(mqttConfig.getHost(), mqttConfig.getPort());
-        } else {
-            bootstrap.bind(mqttConfig.getPort());
-        }
-    }
-
-    /**
+    */
+/**
      * 转发服务端
-     */
+     *//*
+
     private void relayServer() {
         ServerBootstrap bootstrap = new ServerBootstrap().group(bossGroup, workerGroup).channel(mqttConfig.getUseEpoll() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
                 .handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
@@ -132,4 +111,36 @@ public class RelayServerConfiguration {
         }
     }
 
+    */
+/**
+     * 转发客户端
+     *//*
+
+    private void relayClient() {
+        Bootstrap bootstrap = new Bootstrap().group(bossGroup, workerGroup).channel(mqttConfig.getUseEpoll() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel channel) {
+                        ChannelPipeline channelPipeline = channel.pipeline();
+                        // Netty心跳机制
+                        channelPipeline.addLast("idle", new IdleStateHandler(0, 0, mqttConfig.getKeepAlive()));
+                        channelPipeline.addLast("autoflush", new AutoFlushHandler(1, TimeUnit.SECONDS));
+                        channelPipeline.addLast("decoder", new MqttDecoder());
+                        channelPipeline.addLast("encoder", MqttEncoder.INSTANCE);
+                        channelPipeline.addLast("broker", mqttMessageHandler);
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, mqttConfig.getSoBacklog())
+                .option(ChannelOption.SO_REUSEADDR, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childOption(ChannelOption.SO_KEEPALIVE, mqttConfig.getSoKeepAlive())
+                .childOption(ChannelOption.WRITE_BUFFER_WATER_MARK, WriteBufferWaterMark.DEFAULT);
+        if (Strings.isNotBlank(mqttConfig.getHost())) {
+            bootstrap.bind(mqttConfig.getHost(), mqttConfig.getPort());
+        } else {
+            bootstrap.bind(mqttConfig.getPort());
+        }
+    }
+
 }
+*/
